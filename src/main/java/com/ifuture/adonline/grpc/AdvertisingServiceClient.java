7b6640841fd2fed5636f20663a54c675358bcb9c
yongchongwu <yongchongwu@126.com>
2017-09-19 18:36:16 +0800

    initialization

---
+++ src/main/java/com/ifuture/adonline/grpc/AdvertisingServiceClient.java
@@ -0,0 +1,35 @@
+package com.ifuture.adonline.grpc;
+
+import com.ifuture.adonline.AdvertisingServiceGrpc;
+import com.ifuture.adonline.Ifuture.AdvRequest;
+import com.ifuture.adonline.Ifuture.AdvResponse;
+import io.grpc.ClientInterceptors;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import java.util.concurrent.TimeUnit;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertisingServiceClient {
+
+  public static void main(String[] args) throws InterruptedException {
+    SpringApplication.run(AdvertisingServiceClient.class, args);
+    ManagedChannel mChannel = ManagedChannelBuilder.forAddress("localhost", 6565).usePlaintext(true)
+        .build();
+    AdvertisingServiceGrpc.AdvertisingServiceBlockingStub stub = AdvertisingServiceGrpc
+        .newBlockingStub(
+            ClientInterceptors.intercept(mChannel, new AdvertisingClientInterceptor()));
+    try {
+      for (int i = 0; i < 10; i++) {
+        String maid = "" + (i + 1);
+        AdvRequest request = AdvRequest.newBuilder().setMaid(maid).build();
+        AdvResponse response = stub.getAdvertisement(request);
+        System.out.println("返回广告内容:" + response.getAdid());
+        Thread.sleep(1000);
+      }
+    } finally {
+      mChannel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+  }
+}
---
+++ src/main/java/com/ifuture/adonline/grpc/GreeterServiceClient.java
@@ -0,0 +1,30 @@
+package com.ifuture.adonline.grpc;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import java.util.concurrent.TimeUnit;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreeterServiceClient {
+
+  public static void main(String[] args) throws InterruptedException {
+    SpringApplication.run(AdvertisingServiceClient.class, args);
+    ManagedChannel mChannel = ManagedChannelBuilder.forAddress("localhost", 6565).usePlaintext(true)
+        .build();
+    GreeterServiceGrpc.GreeterServiceBlockingStub stub = GreeterServiceGrpc
+        .newBlockingStub(mChannel);
+    try {
+      for (int i = 0; i < 1000; i++) {
+        HelloRequest request = HelloRequest.newBuilder().setName("Jack"+(i+1)).build();
+        HelloReply reply = stub.sayHello(request);
+        System.out.println(reply.getMessage());
+        Thread.sleep(1000);
+      }
+    } finally {
+      mChannel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+  }
+
+}
