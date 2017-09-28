8a26df48036ffd0b40973ef92474c027b498409c
yongchongwu <yongchongwu@126.com>
2017-09-28 15:52:56 +0800

    modify test

--- src/main/java/com/ifuture/adonline/grpc/AdvertisingServiceClient.java
+++ src/main/java/com/ifuture/adonline/grpc/AdvertisingServiceClient.java
@@ -22,32 +22,20 @@ public class AdvertisingServiceClient {
     AdvertisingServiceGrpc.AdvertisingServiceBlockingStub stub = AdvertisingServiceGrpc
         .newBlockingStub(
             ClientInterceptors.intercept(mChannel, new AdvertisingClientInterceptor()));
-
     try {
-      final long startTime = System.nanoTime();
-
-      for (int i = 0; i < 10; i++) {
-        AdvRequest.Builder builder = AdvRequest.newBuilder();
-        builder.setMaid("" + (i + 1));// 用户ID
-        builder.setBussinessId("2");// 商家ID
-        builder.setUa("3");// User-Agent的信息
-        builder.setIp("4");// 交易时的IP
-        builder.setPayMethond("5");// 交易的付账方式
-        builder.setPay(6);// 交易金额，单位为分
-        builder.setNetworkId("7");// 用户的网络
-        AdvRequest request = builder.build();
-        AdvResponse response = stub.getAdvertisement(request);
-        System.out.println("返回广告内容:" + response.getAdid());
-        //Thread.sleep(5000);
-      }
-
-      final long endTime = System.nanoTime();
-
-      System.out.println("time consuming : " + (endTime - startTime) / 1000000L + "ms");
-
+      AdvRequest.Builder builder = AdvRequest.newBuilder();
+      builder.setMaid("1");// 用户ID
+      builder.setBussinessId("2");// 商家ID
+      builder.setUa("3");// User-Agent的信息
+      builder.setIp("4");// 交易时的IP
+      builder.setPayMethond("5");// 交易的付账方式
+      builder.setPay(6);// 交易金额，单位为分
+      builder.setNetworkId("4g");// 用户的网络
+      AdvRequest request = builder.build();
+      AdvResponse response = stub.getAdvertisement(request);
+      System.out.println("返回广告内容:" + response.getAdid());
     } finally {
       mChannel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
     }
-
   }
 }
