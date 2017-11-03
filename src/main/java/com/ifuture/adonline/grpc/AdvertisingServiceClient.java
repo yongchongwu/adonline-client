4b9f6d33e485e70a673ecf9fe2e20cdfce1156a4
yongchongwu <yongchongwu@126.com>
2017-11-03 15:48:54 +0800

    modify grpc client code

--- src/main/java/com/ifuture/adonline/grpc/AdvertisingServiceClient.java
+++ src/main/java/com/ifuture/adonline/grpc/AdvertisingServiceClient.java
@@ -23,11 +23,11 @@ public class AdvertisingServiceClient {
     AdvRequest.Builder builder = AdvRequest.newBuilder();
     builder.setMaid("1");// 用户ID
     builder.setBussinessId("2");// 商家ID
-    builder.setUa("3");// User-Agent的信息
+    builder.setUa("ios");// User-Agent的信息
     builder.setIp("4");// 交易时的IP
     builder.setPayMethond("5");// 交易的付账方式
     builder.setPay(6);// 交易金额，单位为分
-    builder.setNetworkId("4g");// 用户的网络
+    builder.setNetworkId("3g");// 用户的网络
     AdvRequest request = builder.build();
 
     try {
